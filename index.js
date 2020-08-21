379b2c8a28c574e309f221fc26e7b3b9986f689b
Joshua McArthur <jcmcarth06@gmail.com>
2020-08-21 03:05:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ class Cat {
 };
 
 class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  };
 
+  speak() {
+    return `${this.name} says woof!`
+  };
 };
 
 class Bird {
